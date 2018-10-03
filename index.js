bcc2addd75a8952dc62426c1ff409036be981ae7
Conor James Giles <cgiles290@gmail.com>
2018-10-03 11:14:25 +0100

    Added checkable items to populate lists

--- index.js
+++ index.js
@@ -6,10 +6,12 @@ const notes = require('./routes/notes');
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-app.get('/', (req,res) => res.send('It\'s Alive!'))
 app.get('/notes', notes.getAll)
 app.get('/notes/:id', notes.getNote)
 app.post('/notes', notes.newNote)
 app.delete('/notes/:id', notes.dropNote)
+app.post('/notes/:id', notes.newItem)
+app.put('/notes/:id/:label', notes.checkItem)
+app.delete('/notes/:id:/:label', notes.dropItem)
 
 app.listen(port, ()=> console.log(`App listening on port ${port}!`))
--- routes/notes.js
+++ routes/notes.js
@@ -7,13 +7,21 @@ function get(id){
 	let result = notes.filter((x)=>{return x.id == id})
 	return result?result[0]:null
 }
+function getItem(id,label){
+	let note = get(id);
+	let item = note?note.list.filter((x)=>{return x.label == label}):null
+	return item?item[0]:null
+}
 router.getAll = (req,res) => {
 	res.setHeader('Content-Type', 'application/json');
 	res.json(notes);
 }
 router.getNote = (req, res) => {
-	let result = get(req.params.id)
-	res.send(result)
+	let note = get(req.params.id)
+	if(!note)
+		res.send("Could not find note!")
+	else
+		res.send(note)
 }
 router.newNote = (req,res) => {
 	let note = {id: uuid(), list: []}
@@ -27,6 +35,34 @@ router.dropNote = (req,res) => {
 	else{
 		notes.splice(notes.indexOf(note),1)
 		res.redirect('/notes')
+	}
+}
+router.newItem = (req,res) => {
+	let note = get(req.params.id)
+	if(!note)
+		res.send("Could not find note to edit!")
+	else{
+		note.list.push({label: req.body.label, done:false})
+		res.redirect('/notes/' + req.params.id)
+	}
+}
+router.checkItem = (req,res) => {
+	let item = getItem(req.params.id, req.params.label)
+	if(!item)
+		res.send("Could not find item to check/uncheck!")
+	else{
+		item.done = !item.done
+		res.redirect('/notes/'+ req.params.id)
+	}
+}
+router.dropItem = (req,res) => {
+	let note = get(req.params.id)
+	let item = getItem(req.params.id, req.params.label)
+	if(!item)
+		res.send("Could not find item to delete!")
+	else{
+		note.list.splice(note.list.indexOf(item),1)
+		res.redirect('/notes/' + req.params.id)
 	}
 }
 module.exports = router;
